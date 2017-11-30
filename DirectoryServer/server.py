76bba6e780ae2db375181a15b618fd5f4ef1a236
yimk <yimk@tcd.ie>
2017-11-30 18:03:25 +0000

    made some change on server -- ds/auth

--- DirectoryServer/server.py
+++ DirectoryServer/server.py
@@ -13,21 +13,23 @@ def assign_upload_directory():
     # retrieve essential information
     data = request.get_json(force=True)
     user_id = data.get('id')
-    file_server = data.get('file_server')
-    security_check = data.get('security_check')
+    encrypted_file_name = data.get('filename')
+    encrypted_client_pbk= data.get('access_key')
+
+
+    # decrypt the encrypted_key with auth server's private key
+    # this helps client to ensure we are the right auth server, not man-in-middle
+    client_pbk = security_helper.decrypt(encrypted_client_pbk, constant.PRIVATE_KEY)
+    file_name = security_helper.decrypt(encrypted_file_name, client_pbk)
+
 
     """
-    Security Check, on the authentication server side, we do need to worry about the fake Client issue. To make authetication server trust our request,
-    we upload the encrypted public key of auth server(encrypted with client's private key), the authentication server will decrypt(with out client's public key) and compare the public key
-    This ensures the client 
-        - has auth server's public key
-        - has target client's private key
+    if file name already exists
+    the file system will overwrite the old file with the new file
+    however, the file system will inform that to the client with the response
     """
-    client_pbk = constant.CLIENT_PUBLIC_KEY[user_id]
-    fs_pbk = constant.FILE_SERVER_PUBLIC_KEY[file_server]
-    security_check = security_helper.decrypt(security_check, client_pbk)
-
-    if not security_check == constant.PUBLIC_KEY:
+    
+    if not :
         return None
     else:
 
